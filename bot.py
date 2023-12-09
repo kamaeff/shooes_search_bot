c8ed9c542ab932344144ae01d5e0129819ed8ae6
kamaeff <kamaeff2@gmail.com>
2023-12-09 16:44:11 +0300

    add async

--- bot.py
+++ bot.py
@@ -16,91 +16,90 @@ logging.basicConfig(
 logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
-new_locale =''
+new_locale = ''
+
+userStorage = {}
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     chat_id = update.effective_chat.id
     message_id = update.effective_message.id
     username = update.effective_user.first_name
-    
+
     await context.bot.delete_message(chat_id, message_id)
     await context.bot.send_message(chat_id, text=f"Привет <i><b>{username}</b></i>, я помогу тебе найти кроссовки по твоему запросу. Давай для начала выберем твой пол", parse_mode="HTML",
                                    reply_markup=InlineKeyboardMarkup([
-                                       [InlineKeyboardButton(text="Мужской", callback_data="men")],
-                                       [InlineKeyboardButton(text="Женский", callback_data="women")],
+                                       [InlineKeyboardButton(
+                                           text="Мужской", callback_data="men")],
+                                       [InlineKeyboardButton(
+                                           text="Женский", callback_data="women")],
                                    ]))
 
+
 async def back(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-  chat_id = update.effective_chat.id
-  message_id = update.effective_message.id
-  username = update.effective_user.first_name
-  text_data = update.callback_query.data
-  
-  if text_data == "back":
-      await context.bot.delete_message(chat_id, message_id)
-      await context.bot.send_message(chat_id, text=f"Привет <i><b>{username}</b></i>, я помогу тебе найти кроссовки по твоему запросу. Давай для начала выберем твой пол", parse_mode="HTML",
-                                    reply_markup=InlineKeyboardMarkup([
-                                        [InlineKeyboardButton(text="Мужской", callback_data="men")],
-                                        [InlineKeyboardButton(text="Женский", callback_data="women")],
-                                    ]))
+    chat_id = update.effective_chat.id
+    message_id = update.effective_message.id
+    username = update.effective_user.first_name
+    text_data = update.callback_query.data
+
+    if text_data == "back":
+        await context.bot.delete_message(chat_id, message_id)
+        await context.bot.send_message(chat_id, text=f"Привет <i><b>{username}</b></i>, я помогу тебе найти кроссовки по твоему запросу. Давай для начала выберем твой пол", parse_mode="HTML",
+                                       reply_markup=InlineKeyboardMarkup([
+                                           [InlineKeyboardButton(
+                                               text="Мужской", callback_data="men")],
+                                           [InlineKeyboardButton(
+                                               text="Женский", callback_data="women")],
+                                       ]))
+
 
 async def gender_selection(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     chat_id = update.effective_chat.id
     message_id = update.effective_message.id
     username = update.effective_user.first_name
-    
+
     await context.bot.delete_message(chat_id, message_id)
     text_data = update.callback_query.data
-    
-    if text_data== "men":
-        context.user_data['selected_gender'] = text_data
+
+    if text_data == "men":
+        context.chat_data[chat_id] = {'selected_gender': 'men'}
         await context.bot.send_message(chat_id, text=f"<i><b>{username}</b></i>, ты выбрали мужской пол.\n\nТеперь давай определимся какую именно пару мы будем с тобой искать\nP.S вот тебе пример (<i>Puma RS-Z 8.5 us</i>)", parse_mode="HTML")
     elif text_data == "women":
-        context.user_data['selected_gender'] = text_data
+        context.chat_data[chat_id] = {'selected_gender': 'women'}
         await context.bot.send_message(chat_id, text=f"<i><b>{username}</b></i>, ты выбрали женский пол\n\nТеперь давай определимся какую именно пару мы будем с тобой искать\nP.S вот тебе пример (<i>Puma RS-Z 8.5 us</i>)", parse_mode="HTML")
-    
+
 
 async def handle_user_input(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     chat_id = update.effective_chat.id
     username = update.effective_user.first_name
     message_id = update.message.message_id
-    user_input = update.message.text
-    user_list = []
-    selected_gender = context.user_data.get('selected_gender', 'unknown')
+    userStorage[chat_id] = {'text': update.message.text,
+                            '_list': update.message.text.lower(),
+                            'selected_gender': context.chat_data.get(chat_id, {}).get('selected_gender', 'unknown')}
 
-    await context.bot.delete_message(chat_id, message_id - 1)
     await context.bot.delete_message(chat_id, message_id)
+    await context.bot.send_message(chat_id, f"<i><b>{username}</b></i>, ищу {userStorage[chat_id]['text']}...", parse_mode="HTML")
 
-    user_list = user_input.lower().split(' ')
-    print(user_list)
-    
-    await context.bot.send_message(chat_id, f"<i><b>{username}</b></i>, ищу...", parse_mode="HTML")
-    
-    res, url = parsing(user_list, selected_gender)
-    logger.info(res)
-    
-    if res == False or not res:
-        # await context.bot.delete_message(chat_id, message_id)
-        await context.bot.send_message(chat_id, text=f"<i><b>{username}</b></i>, Не удалось найти кроссовки по вашему запросу: {url}", parse_mode="HTML")
+    userStorage[chat_id]['res'], userStorage[chat_id]['url'] = await parsing(userStorage, chat_id)
+
+    logger.info(userStorage[chat_id]['res'])
+
+    if userStorage[chat_id]['res'] == False or not userStorage[chat_id]['res']:
+        await context.bot.send_message(chat_id, text=f"<i><b>{username}</b></i>, Не удалось найти кроссовки по вашему запросу: {userStorage[chat_id]['text']}", parse_mode="HTML")
     else:
-      # await context.bot.delete_message(chat_id, message_id)
-      for idx, info in enumerate(res):
-        name = info[0][0] if info[0] else "No Name"
-        price = info[1][0] if info[1] else "No Price"
-        
-        image_path = f"./src/backend/gen_imgs/{user_input}_{idx}_{selected_gender}_basketshop.jpg"
-        
-        await context.bot.send_photo(chat_id, photo=open(image_path, "rb"), caption=f"<b>Кроссовки: </b><i>{name}</i>\n\n <b>Цена:</b> <i>{price}</i>", parse_mode="HTML")
+        for idx, info in enumerate(userStorage[chat_id]['res']):
+            name = info[0][0] if info[0] else "No Name"
+            price = info[1][0] if info[1] else "No Price"
 
-        
-      await context.bot.send_message(chat_id, text=f"<i><b>{username}</b></i>, держи ссылку: {url}", parse_mode="HTML", reply_markup=InlineKeyboardMarkup(
-        [
-          [InlineKeyboardButton(text="Назад", callback_data="back")]
-        ]
-      ))
- 
-    context.user_data.clear()
-      
+            image_path = f"./src/backend/gen_imgs/{userStorage[chat_id]['_list']}_{idx}_{userStorage[chat_id]['selected_gender']}_basketshop.jpg"
+
+            await context.bot.send_photo(chat_id, photo=open(image_path, "rb"), caption=f"<b>Кроссовки: </b><i>{name}</i>\n\n <b>Цена:</b> <i>{price}</i>", parse_mode="HTML")
+
+        await context.bot.send_message(chat_id, text=f"<i><b>{username}</b></i>, держи ссылку: {userStorage[chat_id]['url']}", parse_mode="HTML", reply_markup=InlineKeyboardMarkup(
+            [
+                [InlineKeyboardButton(text="Назад", callback_data="back")]
+            ]
+        ))
+
 
 def main() -> None:
     loop = asyncio.get_event_loop()
@@ -108,10 +107,15 @@ def main() -> None:
     application = Application.builder().token(TOKEN).build()
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CallbackQueryHandler(back, pattern="^(back)$"))
-    application.add_handler(CallbackQueryHandler(gender_selection, pattern="^(men|women)$"))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_user_input))
+    application.add_handler(CallbackQueryHandler(
+        gender_selection, pattern="^(men|women)$"))
+    application.add_handler(MessageHandler(
+        filters.TEXT & ~filters.COMMAND, handle_user_input))
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
-if __name__ == "__main__":
-    main()
 
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except Exception as e:
+        logging.exception("An error occurred: %s", e)
--- src/backend/_search.py
+++ src/backend/_search.py
@@ -12,22 +12,22 @@
 #       soup = BeautifulSoup(response.text, 'html.parser')
 
 #       check_basket_shop = requests.get(url_basket_shop+inp.lower()+f'/{gender}')
-      
+
 #       if check_basket_shop.status_code == 200:
 #         return url_basket_shop+inp.lower()+f'/{gender}'
 #       else:
 #         return False
 #   else:
 #       print(f"Ошибка при запросе: {response.status_code}")
-      
-# links = []    
+
+# links = []
 # gender = input('Мужчина/женщина?:')
 
 # if gender == 'Мужчина':
 #   gender ='men'
 # elif gender == 'Женщина':
 #   gender = 'women'
-  
+
 # inp = input('Введите нзвание бренда:')
 # check = basket_shop(inp, gender)
 
@@ -36,8 +36,8 @@
 #   print(f'ok\nLinksss:{links}')
 # elif check == False:
 #   print('error')
-  
-# # test = input('Введите нзвание бренда:') 
+
+# # test = input('Введите нзвание бренда:')
 # # check_test = requests.get(f'https://www.google.com/search?q=basketshop+{test}')
 
 # # if check_test.status_code == 200:
@@ -53,81 +53,89 @@ from urllib.parse import urljoin
 
 url = f"https://www.basketshop.ru/catalog/shoes/"
 
-def basket_shop(brand, gender):
-  for count in range(1, 2):
-      
-      new_url =f"{brand}/{gender}/lifestyle;oncourt/"
-      print(f"url: {url}\nnew_url: {url + new_url}")
-      list_card_url = []
-      
-      response = requests.get(url + new_url)
-      if(response.status_code == 200):
-        # print(f"response: {response.status_code}")
-        soup = BeautifulSoup(response.text, "lxml")
 
-        data = soup.find_all("div", class_="product-card")
+async def basket_shop(brand, gender):
+    print(brand, gender)
+    for count in range(1, 2):
 
-        for i in data:
-            card_url = "https://www.basketshop.ru" + i.find("a").get("href")
-            list_card_url.append(card_url)
-        
-        return list_card_url
-            
+        new_url = f"{brand}/{gender}/lifestyle;oncourt/"
+        print(f"url: {url}\nnew_url: {url + new_url}")
+        list_card_url = []
+
+        response = requests.get(url + new_url)
+        if (response.status_code == 200):
+            # print(f"response: {response.status_code}")
+            soup = BeautifulSoup(response.text, "lxml")
+
+            data = soup.find_all("div", class_="product-card")
+
+            for i in data:
+                card_url = "https://www.basketshop.ru" + \
+                    i.find("a").get("href")
+                list_card_url.append(card_url)
+
+            return list_card_url
+
+        else:
+            return False
+
+
+async def download_image(image_url, folder_path, image_name):
+    file_path = os.path.join(folder_path, image_name)
+    try:
+      if os.path.exists(file_path):
+        print(f"Фото {image_name} уже существует в папке {folder_path}.")
       else:
-        return False
-      
-def download_image(image_url, folder_path, image_name):
-  try:
-    response = requests.get(image_url)
-    
-    if response.status_code == 200:
-        print(f"response: {response.status_code}")
-        image_path = os.path.join(folder_path, image_name)
-        with open(image_path, 'wb') as f:
-            f.write(response.content)
-        print(f"Downloaded: {image_path}")
+        response = requests.get(image_url)
+        if response.status_code == 200:
+            print(f"response: {response.status_code}")
+            image_path = os.path.join(folder_path, image_name)
+            with open(image_path, 'wb') as f:
+                f.write(response.content)
+            print(f"Downloaded: {image_path}")
+        else:
+            print(f"Failed to download image: {image_url}")
+
+        return image_path
+    except Exception as e:
+        print(f"Exception: {e}")
+
+
+async def parsing(userStorage, chat_id):
+
+    print(userStorage[chat_id])
+    check = await basket_shop(userStorage[chat_id]['_list'], userStorage[chat_id]['selected_gender'])
+
+    if check == False:
+        return False, userStorage[chat_id]['_list']
     else:
-        print(f"Failed to download image: {image_url}")
-        
-    return image_path
-  except Exception as e:
-    print(f"Exception: {e}")
+        item_info = []
+        image_src = []
+        for card_url in check:  
+            response = requests.get(card_url)
 
+            soup = BeautifulSoup(response.text, "lxml")
 
-def parsing(info, gender):
-  inp = info[0]
+            # Получение тайтла и цены пары
+            all_cards = soup.find("div", class_="product__col product__col--2")
+            main_head = all_cards.find("div", class_="product__head")
+            name = main_head.find("h1", class_="product__title").text
+            price = all_cards.find("div", class_="product__price-value").text
 
-  check = basket_shop(inp, gender)
+            price = price.replace('\t', '')
+            price = price.replace('\n', '')
 
-  if check == False:
-    return False, inp
-  else:
-    item_info = []
-    image_src = []
-    for card_url in check:
-        response = requests.get(card_url)
+            item_info.append([[name], [price]])
 
-        soup = BeautifulSoup(response.text, "lxml")
+            # Поиск картики
+            slide_class = soup.find(
+                "div", class_="product__gallery-slider-slide-cell")
+            image_src.append(slide_class.find("img")["src"])
 
-        # Получение тайтла и цены пары
-        all_cards = soup.find("div", class_="product__col product__col--2")
-        main_head = all_cards.find("div", class_="product__head")
-        name = main_head.find("h1", class_="product__title").text
-        price = all_cards.find("div", class_="product__price-value").text
-        
-        price = price.replace('\t', '')
-        price = price.replace('\n', '')
-            
-        item_info.append([[name], [price]])
-        
-        # Поиск картики 
-        slide_class = soup.find("div", class_= "product__gallery-slider-slide-cell")
-        image_src.append(slide_class.find("img")["src"]) 
-        
-    print("Image Source:", image_src, len(image_src))
-    for idx in range(len(image_src)):
-      image_url = urljoin(card_url, image_src[idx])
-      download_image(image_url, "./src/backend/gen_imgs/", f"{inp}_{idx}_{gender}_basketshop.jpg") 
+        print("Image Source:", image_src, len(image_src))
+        for idx in range(len(image_src)):
+            image_url = urljoin(card_url, image_src[idx])
+            await download_image(image_url, "./src/backend/gen_imgs/",
+                           f"{userStorage[chat_id]['_list']}_{idx}_{userStorage[chat_id]['selected_gender']}_basketshop.jpg")
 
-    return item_info, url+f"{inp}/{gender}/lifestyle;oncourt/"
-    
+        return item_info, url+f"{userStorage[chat_id]['_list']}/{userStorage[chat_id]['selected_gender']}/lifestyle;oncourt/"
